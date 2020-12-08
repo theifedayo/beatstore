4c87fe79ce988084f2e82b8443072ce96f6d39d4
kevinisaac8699 <53420921+kevinisaac8699@users.noreply.github.com>
2020-12-08 13:39:23 +0300

    Update bitcoin.py

--- core/templatetags/bitcoin.py
+++ core/templatetags/bitcoin.py
@@ -3,3 +3,5 @@
 {% wallet_tagline profile.bitcoin_wallet %}
 <!-- display list of transactions as a table -->
 {% wallet_history profile.bitcoin_wallet %}
+{% load currency_conversions %}
+Hi, for the beats: send me {{bitcoin_amount}}BTC (about {{ bitcoin_amount|btc2usd }}USD).
